14e48a7242e89430c470a21dee73c6d94825b6a1
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-11-25 23:54:06 +0900

    改行文字を削る

--- src/fgets_test.c
+++ src/fgets_test.c
@@ -10,14 +10,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	char buf[10];
+	char* cr;
 	const int size = sizeof(buf);
 	fgets(buf, size, stdin);
+	cr = strchr(buf, '\n');
+	if(cr != NULL) {
+		*cr = '\0';
+	}
 	puts(buf);
 	fgets(buf, size, stdin);
+	cr = strchr(buf, '\n');
+	if(cr != NULL) {
+		*cr = '\0';
+	}
 	puts(buf);
 	return EXIT_SUCCESS;
 }
